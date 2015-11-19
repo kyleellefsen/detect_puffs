8039151be629208179de6a6015fd388a728dfb63
kyleellefsen <kellefse@uci.edu>
2015-11-19 10:53:57 -0800

    fixed init file

--- detect_puffs/__init__.py
+++ detect_puffs/__init__.py
@@ -10,6 +10,6 @@ date='11/15/2015'
 dependencies = ['skimage', 'leastsqbound', 'matplotlib','PyOpenGL']
 
 menu_layout = {'Detect Puffs': \
-				{'Threshold Cluster': ['threshold_cluster', 'threshold_cluster.gui']},\
-				{'Simulate Puffs': ['puff_simulator.puff_simulator', 'simulate_puffs.gui']}
+				{'Threshold Cluster': ['threshold_cluster', 'threshold_cluster.gui'],
+				'Simulate Puffs': ['puff_simulator.puff_simulator', 'simulate_puffs.gui']}
 				}
